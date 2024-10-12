c3a4374efa26b4bcdac7227eb349b8df98a2fd92
bmdx26 <poseidonx26@gmail.com>
2024-10-12 16:15:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-//LOADING SCREEN
-
-
+// LOADING SCREEN
 window.addEventListener('load', function() {
-  
   setTimeout(function() {
-      
       document.getElementById('loading').style.display = 'none';
-      
       document.getElementById('content').style.display = 'block';
-  }, 3000); 
+  }, 3000);
 });
 
-
-
-//HAMBURGER MENU
-
+// HAMBURGER MENU
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.nav-links');
+const closeMenu = document.getElementById('close-menu');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
-  hamburger.classList.toggle('active'); 
+  hamburger.classList.toggle('active');
+  closeMenu.style.display = navLinks.classList.contains('active') ? 'block' : 'none'; n
+});
+
+// HAMBURGER MENU CLOSE BUTTON
+closeMenu.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+  hamburger.classList.remove('active');
+  closeMenu.style.display = 'none'; 
 });
